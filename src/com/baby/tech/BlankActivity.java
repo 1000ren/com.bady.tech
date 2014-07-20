6c9e3dfac5796f22f8464e586c2403dcc789d1fd
1000ren <fanwei1207@163.com>
2014-07-20 13:49:53 +0800

    测试

--- src/com/baby/tech/BlankActivity.java
+++ src/com/baby/tech/BlankActivity.java
@@ -11,6 +11,7 @@ public class BlankActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//
 		setContentView(R.layout.activity_blank);
 	}
 
